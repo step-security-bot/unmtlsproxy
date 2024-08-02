89f0e459ffd4a412e9d8c19d16753e321ca23877
Ajabep <ajabep@tutanota.com>
2024-08-02 12:12:27 +0200

    Remove old code I forgot to remove

--- unmtlsproxy.go
+++ unmtlsproxy.go
@@ -53,20 +53,6 @@ func main() {
 		// Exchange
 		KeyLogWriter:  w,
 		Renegotiation: tls.RenegotiateFreelyAsClient,
-
-		/// OLD
-		ClientAuth:               tls.RequireAndVerifyClientCert,
-		MinVersion:               tls.VersionTLS12,
-		SessionTicketsDisabled:   true,
-		PreferServerCipherSuites: true,
-
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-		},
 	}
 
 	switch cfg.Mode {
